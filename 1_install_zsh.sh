36c214d2e1bbcd02cee5161f8e457b21737204d0
Mariusz Krotos <mar.krotos@gmail.com>
2024-04-12 22:40:27 +0200

    Add shell choose

--- 1_install_zsh.sh
+++ 1_install_zsh.sh
@@ -1,13 +1,32 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 
 # System update
 sudo apt update && sudo apt upgrade -y
 
+echo "Zsh or Fish?"
+echo "1: Zsh (default)"
+echo "2: Fish"
+read -r which_shell
 
-# Install zsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [[ $which_shell == 2 ]]; then
+	# Fish shell install - crashes when installed with snap
+	sudo apt-add-repository ppa:fish-shell/release-3
+	sudo apt update
+	sudo apt install fish -y
+	mkdir ~/.config/fish/functions
+	cp ./fish_functions/* ~/.config/fish/functions/
+
+	# SDK man support for the fish shell
+	sudo fisher install reitzig/sdkman-for-fish@v1.4.0
+else
+	# Install zsh
+	sudo apt install zsh -y
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Have to reboot to apply changes
 echo 'Reboot now to apply changes...'
+read -rn1 -p "Reboot? Y/n " reboot
+if [[ $reboot != n ]]; then
+	reboot
+fi
--- mantra.sh
+++ mantra.sh
@@ -1,9 +1,7 @@
-#!/bin/zsh
-
+#!/usr/bin/env bash
 
 # System update
 sudo apt update && sudo apt upgrade -y
-
 
 # Installing nvidia drivers: https://phoenixnap.com/kb/install-nvidia-drivers-ubuntu
 # sudo add-apt-repository ppa:graphics-drivers/ppa
@@ -11,7 +9,6 @@ sudo apt update && sudo apt upgrade -y
 # sudo ubuntu-drivers autoinstall
 # or
 # sudo apt install firmware-linux -y #drivers
-
 
 # Common packages install
 sudo apt install tree -y
@@ -23,12 +20,11 @@ sudo apt install locate -y
 sudo apt install vim -y
 sudo apt install curl -y
 
-
-echo "source ~/.aliases" >> ~/.zshrc
+echo "source ~/.aliases" >>~/.zshrc
 touch ~/.aliases
 
 mkdir -p ~/.local/bin
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.zshrc
+echo 'export PATH=$HOME/.local/bin:$PATH' >>~/.zshrc
 
 # SDK man install
 sudo curl -s "https://get.sdkman.io" | bash
@@ -37,17 +33,6 @@ sdk version
 sdk update
 sdk install java
 sdk install kotlin
-
-# Fish shell install - crashes when installed with snap
-#sudo apt-add-repository ppa:fish-shell/release-3
-#sudo apt update
-#sudo apt install fish -y
-# mkdir ~/.config/fish/functions
-# cp ./fish_functions/* ~/.config/fish/functions/
-
-# SDK man support for the fish shell
-# sudo fisher install reitzig/sdkman-for-fish@v1.4.0
-
 
 # Install snapd's for ubuntu
 sudo apt install snapd -y
@@ -59,13 +44,6 @@ sudo snap install node --classic
 sudo snap install chromium
 sudo snap install mailspring
 sudo snap install bitwarden
-
-
-# Install nodejs 17 - manual
-# curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-# sudo apt update
-# sudo apt-get install -y nodejs
-
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
